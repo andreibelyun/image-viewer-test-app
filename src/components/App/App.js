d36a8c61613044f0d879a7d2f71e9ad19b7c01f8
Andrey Belyun <andreibelyun@yandex.com>
2023-01-20 13:59:15 +0300

    feat: create private route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,31 @@
 import "./App.scss";
 import Login from "../Login/Login";
-
 import ImageView from "../ImageView/ImageView";
 import { Routes, Route } from "react-router-dom";
 import Main from "../Main/Main";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Main />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/image-view"
+          element={
+            <PrivateRoute>
+              <ImageView />
+            </PrivateRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
-        <Route path="/image-view" element={<ImageView />} />
       </Routes>
     </div>
   );
